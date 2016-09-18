f12212624587df723e7c365b2b488da9abe902ad
Agustin Chiappe Berrini <jnieve@gmail.com>
2016-09-18 18:28:13 -0400

    Adjusting test script to images

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+docker pull ubuntu:latest
 ./node_modules/standard/bin/cmd.js src
 mkdir test_tmp
 ./node_modules/babel-cli/bin/babel.js test --presets babel-preset-es2015 --out-dir test_tmp

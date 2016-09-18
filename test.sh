c924486b5f4a9e4e5c07c8d31f73bbc4f776c760
Agustin Chiappe Berrini <jnieve@gmail.com>
2016-09-18 18:29:57 -0400

    Simplifying test script

--- test.sh
+++ test.sh
@@ -2,9 +2,5 @@
 
 docker pull ubuntu:latest
 ./node_modules/standard/bin/cmd.js src
-mkdir test_tmp
-./node_modules/babel-cli/bin/babel.js test --presets babel-preset-es2015 --out-dir test_tmp
-./node_modules/mocha/bin/mocha --require babel-core/register -R spec test_tmp
-ecode=$?
-rm -fr test_tmp
-exit $ecode
+./node_modules/mocha/bin/mocha --require babel-core/register -R spec
+exit $?

23473e184d24dbb02250a3068efe528b5f787740
Agustin Chiappe Berrini <jnieve@gmail.com>
2016-09-18 17:41:41 -0400

    Fixing language in travis configuration

--- test.sh
+++ test.sh
@@ -4,4 +4,6 @@
 mkdir test_tmp
 ./node_modules/babel-cli/bin/babel.js test --presets babel-preset-es2015 --out-dir test_tmp
 ./node_modules/mocha/bin/mocha --require babel-core/register -R spec test_tmp
+ecode=$?
 rm -fr test_tmp
+exit $ecode
